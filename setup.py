f69e9f1f6a12ff22f344ae31f2a06a2976c2d458
Collin Simon <csimon@talend.com>
2021-10-20 17:19:51 +0000

    Bump to v0.0.2 to update url

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tap-calendly',
-    version='0.0.1',
+    version='0.0.2',
     packages=['tap_calendly', 'tap_calendly.tests'],
     url='https://github.com/singer-io/tap-calendly',
     license='',
