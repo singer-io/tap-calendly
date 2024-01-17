4e7bbf1b48217b904ff753d815b4295e1f4d94ce
Ubuntu <ubuntu@ip-10-10-11-197.ec2.internal>
2024-01-17 19:40:42 +0000

    Remove setuptools from reqs to remove high-sev vulnerabilty

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name='tap-calendly',
-    version='0.0.2',
+    version='0.0.3',
     packages=['tap_calendly', 'tap_calendly.tests'],
     url='https://github.com/singer-io/tap-calendly',
     license='',
     author='Nolan McCafferty',
     author_email='nolanmccafferty@gmail.com',
     description='Singer.io tap for Calendly',
-    install_requires=['setuptools~=41.0.1', 'requests~=2.25.1', 'singer-sdk'],
+    install_requires=[
+        'requests~=2.25.1',
+        'singer-sdk'],
 )
